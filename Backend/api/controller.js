ce4d6c18d34357d5db9224841bfcfd5a90fbe089
Pasindu Prabhath <pasinduprabhath@gmail.com>
2023-08-04 19:58:15 +0530

    pw encrpyption done. ✅

--- Backend/api/controller.js
+++ Backend/api/controller.js
@@ -1,8 +1,26 @@
+const { get } = require('./router');
 const {userRegister,getUserByEmail} = require('./service');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     userRegister: (req, res) => {
-        const body = req.body;
+        const {userName,email,password} = req.body;
+        getUserByEmail(email,(err, results) => {
+            if(err){
+                console.log(err);
+            }
+            if(results.length > 0){
+                return res.json({
+                    success: 0,
+                    message: "Email already exists"
+                });
+            }
+            if (results.length == 0){
+                const saltRounds = 10;
+            const salt = bcrypt.genSaltSync(saltRounds);
+            const encryptedPassword = bcrypt.hashSync(password, salt);
+            const body = {
+                userName,email,password:encryptedPassword}
         userRegister(body, (err, results) => {
             if(err){
                 console.log(err);
@@ -16,7 +34,7 @@ module.exports = {
                 data: results
             });
         });
-    },
+}},)},
     getUserByEmail: (req, res) => {
         const {email} = req.body;
         getUserByEmail(email,(err, results) => {
