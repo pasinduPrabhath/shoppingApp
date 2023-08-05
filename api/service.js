2468dc61ed9f8eeb468b05fa6be82f4adcf9edd7
Pasindu Prabhath <pasinduprabhath@gmail.com>
2023-08-05 22:42:43 +0530

    added otp generation and store in db.

--- api/service.js
+++ api/service.js
@@ -1,17 +1,27 @@
 const pool = require("../config/database");
 const { sendEmail } = require("./email");
 // const { get } = require('./user.router');
+
+function generateOTP() {
+    const digits = "0123456789";
+    let otp = "";
+    for (let i = 0; i < 6; i++) {
+      otp += digits[Math.floor(Math.random() * 10)];
+    }
+    return otp;
+  }
 module.exports = {
   userRegister: (data, callBack) => {
+    const otp = generateOTP();
     pool.query(
-      `insert into userTable(userName,email,password) values(?,?,?)`,
-      [data.userName, data.email, data.password],
+      `insert into userTable(userName,email,password,otp,account_status) values(?,?,?,?,?)`,
+      [data.userName, data.email, data.password, otp, "pending"],
       async (error, results, fields) => {
         if (error) {
           return callBack(error);
         }
         try {
-          await sendEmail(data.email, data.userName, "Welcome to our website!");
+          await sendEmail(data.email, data.userName, "Welcome to the store! \nYou have to verify your account by entering this OTP: " + otp);
           return callBack(null, results);
         } catch (error) {
           console.error(error);
