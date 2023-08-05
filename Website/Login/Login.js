e43075f57fda587d197c414d25e1ff9f950b2485
Nila0820 <91244778+Nila0820@users.noreply.github.com>
2023-08-05 17:57:38 +0530

    Add validations and connect to the backend

--- Website/Login/Login.js
+++ Website/Login/Login.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Login.css';
+import axios from 'axios';
 
 export default function Login() {
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
+      
+        const handleSubmit = async (e) => {
+          e.preventDefault();
+      
+          try {
+            const response = await axios.post(
+              'https://gowild.herokuapp.com/api/user/login',
+              { email, password }
+            );
+      
+            
+            if (response.status === 200) {
+              console.log(response.data);
+            } else {
+              alert('Invalid username or password');
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        };
+
   return (
     <div className='login-container'>
         <div className='log'>
             <h1>Login</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div>
-                <input type="text" id="email" placeholder='Enter the email'/>
+                <input type="text" id="email" placeholder='Enter the email' value={email} onChange={(e) => setEmail(e.target.value)}/>
               </div>
               <br />
               <div>
-                <input type="password" id="password" placeholder='Enter the password'/>
+                <input type="password" id="password" placeholder='Enter the password' value={password} onChange={(e) => setPassword(e.target.value)}/>
               </div>
               <br />
               <div>
@@ -20,7 +44,7 @@ export default function Login() {
               </div>
               <br/>
               <div>
-                <button type="button" className='customerbutton'>
+                <button type="submit" className='customerbutton'>
                   Login
                 </button>
               </div>
