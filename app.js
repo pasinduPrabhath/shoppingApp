6c376f15d84f8e6011aaa0df5d83ed0bee8d1b83
Pasindu Prabhath <pasinduprabhath@gmail.com>
2023-08-05 12:45:25 +0530

    added cors to backend ✅

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors"); 
+app.use(cors());
 // const port = 8000;
 const router = require("./api/router");
 
