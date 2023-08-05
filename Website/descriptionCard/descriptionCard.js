a4f0dd11960aec53387079775732059a619308a3
Manahara <manaharabibu123@gmail.com>
2023-08-05 10:00:50 +0530

    product Description Card added

---
+++ Website/descriptionCard/descriptionCard.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const ProductCard = ({
+  title,
+  description,
+  price,
+  colors,
+}) => {
+  const handleColorClick = (color) => {
+    // Change the color of the product card and its associated elements.
+    document.body.style.backgroundColor = color;
+    const activeColor = document.querySelector(`.product-colors .active`);
+    activeColor.classList.remove("active");
+    color.classList.add("active");
+  };
+
+  return (
+    <div className="container">
+      <div className="imgBx">
+        <img src={colors.active.dataPic} alt="Nike Jordan Proto-Lyte Image" />
+      </div>
+      <div className="details">
+        <div className="content">
+          <h2>{title}</h2>
+          <p>{description}</p>
+          <p className="product-colors">Available Colors:
+            {colors.map((color) => (
+              <span
+                key={color.id}
+                className={color.active ? "active" : ""}
+                onClick={() => handleColorClick(color)}
+                data-color-primary={color.dataColorPrimary}
+                data-color-sec={color.dataColorSec}
+                data-pic={color.dataPic}
+              >
+                {color.name}
+              </span>
+            ))}
+          </p>
+          <h3>Rs. {price}</h3>
+          <button>Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
