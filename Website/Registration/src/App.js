9151c53feeb4109c5b706eb29f22f3c46c881525
Nila0820 <91244778+Nila0820@users.noreply.github.com>
2023-08-05 21:04:31 +0530

    connect to the backend

--- Website/Registration/src/App.js
+++ Website/Registration/src/App.js
@@ -1,9 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
 import profile from "./images/a.png";
 import email from "./images/email.jpg";
 import pass from "./images/pass.png";
+import axios from 'axios';
 
 function App() {
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [confirmPasswordValue, setConfirmPasswordValue] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'https://lapshopapp-f26f1576abb1.herokuapp.com/api/register',
+        { email: emailValue, password: passwordValue, confirmPassword: confirmPasswordValue }
+      );
+
+    
+      console.log(response.data);
+    } catch (error) {
+    
+      console.error(error);
+    }
+  };
   return (
     <div className="main">
      <div className="sub-main">
@@ -13,10 +35,10 @@ function App() {
              <img src={profile} alt="profile" className="profile"/>
            </div>
 
-
          </div>
          <div>
            <h1>Registration</h1>
+           <form onSubmit={handleSubmit}>
            <div>
              <img src={email} alt="email" className="email"/>
              <input type="text" placeholder="Email" className="name"/>
@@ -27,15 +49,17 @@ function App() {
            </div>
            <div className="third-input">
              <img src={pass} alt="pass" className="email"/>
-             <input type="password" placeholder="Confirm password" className="name"/>
+             <input type="password" placeholder="Confirm password" className="name" value={confirmPasswordValue} onChange={(e) => setConfirmPasswordValue(e.target.value)}/>
            </div>
           <div className="signup-button">
           <button>Create Account</button>
           </div>
+         
            
             <p className="link">
               <a href="#">Forgot password ?</a> Or <a href="#">Login</a>
             </p>
+            </form>
            
  
          </div>
