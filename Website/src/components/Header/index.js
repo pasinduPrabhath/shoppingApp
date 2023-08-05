fd5c7576a1ba846a198042f1567adef0f99eb122
isuru lakmal <isurulakmal200013@gmail.com>
2023-08-05 15:05:12 +0530

    small modifications done

--- Website/src/components/Header/index.js
+++ Website/src/components/Header/index.js
@@ -30,7 +30,7 @@
 import React from "react";
 import icon from "../../icon/laptop.png";
 import cart from "../../icon/cart.png";
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -38,12 +38,12 @@ const Header = () => {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <img src={icon} alt="icon" className="w-10 h-10 mr-2" />{" "}
-          <span className="ml-3 text-xl">Lapzoid</span>
+          <span className="ml-3 text-xl items-center">Lapzoid</span>
         </a>
 
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
           <a className="mr-5 hover:text-gray-900">Product</a>
-          <Link to="/product" activeClassName="text-red-500" className="mr-5 hover:text-gray-900">DescriptionCard</Link>
+          {/* <Link to="/product" activeClassName="text-red-500" className="mr-5 hover:text-gray-900">DescriptionCard</Link> */}
           <a className="mr-5 hover:text-gray-900">Third Link</a>
           <a className="mr-5 hover:text-gray-900">Fourth Link</a>
         </nav>
