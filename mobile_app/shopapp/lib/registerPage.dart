00ce4b1c55c34b8c92a9a6ac1fe59c71a184be88
Tharindu Adhikari <88026851+TharinduAdhikari@users.noreply.github.com>
2023-08-05 16:29:10 +0530

    Registration page

---
+++ mobile_app/shopapp/lib/registerPage.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:fresh_store_ui/loginPage.dart';
+
+class MyRegister extends StatefulWidget {
+  const MyRegister({Key? key}) : super(key: key);
+
+  @override
+  _MyRegisterState createState() => _MyRegisterState();
+}
+
+class _MyRegisterState extends State<MyRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        backgroundColor: Color.fromARGB(128, 113, 113, 113),
+        body: Stack(children: [
+          Container(
+            padding: const EdgeInsets.only(left: 35, top: 50),
+            child: const Text(
+              "Create\nAccount",
+              style: TextStyle(color: Colors.white, fontSize: 33),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+                  right: 35,
+                  left: 35,
+                  top: MediaQuery.of(context).size.height * 0.32),
+              child: Column(children: [
+                TextField(
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    hintText: 'Name',
+                    hintStyle: const TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    hintText: 'Email',
+                    hintStyle: const TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    hintText: 'Password',
+                    hintStyle: const TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                ),
+                const SizedBox(
+                  height: 180,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 27,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: const Color(0xff4c505b),
+                        child: IconButton(
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => MyLogin()),
+                            );
+                          },
+                          icon: const Icon(Icons.arrow_forward),
+                        ),
+                      ),
+                    ]),
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: []),
+              ]),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
