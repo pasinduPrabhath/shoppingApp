7be658fea0373f08cdca70d689227beac267f132
Pasindu Prabhath <pasinduprabhath@gmail.com>
2023-08-06 16:32:22 +0530

    .

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const router = require("./api/router");
 
 app.use(express.json());
 app.use("/api", router);
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
