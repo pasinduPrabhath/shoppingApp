f2a47a564658cd759c84a815a66180f3c6a6f8f6
isuru lakmal <isurulakmal200013@gmail.com>
2023-08-06 13:32:54 +0530

    registration page bug fixed-null

--- Website/src/components/descriptionCard/descriptionCard.js
+++ Website/src/components/descriptionCard/descriptionCard.js
@@ -40,7 +40,7 @@ const ProductSection = () => {
           product_id: product_id,
         }
       );
-      console.log(response.data); // Handle the response as needed
+      console.log(response.data);
       setSuccessAlert(true);
       setErrorAlert(false);
     } catch (error) {
@@ -71,15 +71,13 @@ const ProductSection = () => {
               {product.title}
             </h1>
             <p className="leading-relaxed">{product.description}</p>
-            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-              {/* Color and Size elements go here */}
-            </div>
+            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5"></div>
             <div className="flex">
               <span className="title-font font-medium text-2xl text-gray-900">
                 ${product.price}
               </span>
               <button
-                onClick={handleAddToCart} // Call handleAddToCart when the button is clicked
+                onClick={handleAddToCart}
                 className="flex ml-auto text-white bg-red-500 border-0 py-2 px-20 focus:outline-none hover:bg-red-700 rounded"
               >
                 Add to cart
