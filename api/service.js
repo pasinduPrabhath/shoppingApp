7a16e498c1a65bb57ceadef743ca60b545fc51c1
Pasindu Prabhath <pasinduprabhath@gmail.com>
2023-08-06 12:18:32 +0530

    .

--- api/service.js
+++ api/service.js
@@ -34,7 +34,7 @@ userRegister: (data, callBack) => {
 getUserByEmail: (email, callBack) => {
     pool.query(
       `select 
-        userId,userName,email,account_status
+        userId,userName,email,account_status,password
         from userTable
         where
         email = ?`,
